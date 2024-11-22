a9fd5e15c520b88173cffab353fd680d546c112e
lumleon <lumleon@yahoo.com>
2024-11-22 17:08:34 +0800

    Update index.js with debug

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+console.log('Index.js loaded');
+
 const container = document.getElementById('chrome-extension-root');
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (container) {
+  console.log('Found chrome-extension-root');
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('chrome-extension-root not found');
+}
